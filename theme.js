0961a81f82eb2bc3c3e520e0cd7fdd20249a41be
JungVictor <victor.jung@etu.unice.fr>
2021-12-17 21:04:21 +0100

    Change color scheme of the theme button

--- theme.js
+++ theme.js
@@ -4,12 +4,12 @@ function toggleTheme(element){
   if(css.href == "https://cdn.jsdelivr.net/npm/docsify-themeable@0/dist/css/theme-simple-dark.css") {
     css.href = "https://cdn.jsdelivr.net/npm/docsify-themeable@0/dist/css/theme-simple.css";
     element.innerHTML = 'Dark Theme';
-    element.style.backgroundColor = "rgba(0,0,0,0.1)"
-    element.style.border = "1px solid rgba(0, 0, 0, 0.2)"
+    element.style.backgroundColor = "#f7f7f7"
+    element.style.border = "1px solid #e3e3e3"
   } else {
     css.href = "https://cdn.jsdelivr.net/npm/docsify-themeable@0/dist/css/theme-simple-dark.css";
     element.innerHTML = 'Light Theme';
-    element.style.backgroundColor = "rgba(0,0,0,0.3)"
-    element.style.border = "1px solid rgba(0, 0, 0, 0.5)"
+    element.style.backgroundColor = "#242e33"
+    element.style.border = "1px solid #34434b"
   }
 }
