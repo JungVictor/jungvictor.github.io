409e6fa9c976860a21d78c311190b9bb29ffc90d
JungVictor <victor.jung@etu.unice.fr>
2021-12-17 21:00:45 +0100

    Change the theme

--- theme.js
+++ theme.js
@@ -3,9 +3,13 @@ function toggleTheme(element){
   // Dark theme
   if(css.href == "https://cdn.jsdelivr.net/npm/docsify-themeable@0/dist/css/theme-simple-dark.css") {
     css.href = "https://cdn.jsdelivr.net/npm/docsify-themeable@0/dist/css/theme-simple.css";
-    element.innerHTML = '<img id="theme-switch" src="images/dark.svg">';
+    element.innerHTML = 'Dark Theme';
+    element.style.backgroundColor = "rgba(0,0,0,0.1)"
+    element.style.border = "1px solid rgba(0, 0, 0, 0.2)"
   } else {
     css.href = "https://cdn.jsdelivr.net/npm/docsify-themeable@0/dist/css/theme-simple-dark.css";
-    element.innerHTML = '<img id="theme-switch" src="images/light.svg">';
+    element.innerHTML = 'Light Theme';
+    element.style.backgroundColor = "rgba(0,0,0,0.3)"
+    element.style.border = "1px solid rgba(0, 0, 0, 0.5)"
   }
 }
