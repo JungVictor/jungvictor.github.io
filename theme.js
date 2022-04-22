cb41e33684f1d11eef0f759e09caf83aa730455c
JungVictor <victor.jung@etu.unice.fr>
2022-04-22 12:56:53 +0200

    Custom colors

--- theme.js
+++ theme.js
@@ -4,13 +4,13 @@ function toggleTheme(element){
   // Dark theme
   if(css.href == "https://cdn.jsdelivr.net/npm/docsify-themeable@0/dist/css/theme-simple-dark.css") {
     css.href = "https://cdn.jsdelivr.net/npm/docsify-themeable@0/dist/css/theme-simple.css";
-    color.href="color-light.css";
+    color.href = "color-light.css";
     element.innerHTML = 'Dark Theme';
     element.style.backgroundColor = "#f7f7f7"
     element.style.border = "1px solid #e3e3e3"
   } else {
     css.href = "https://cdn.jsdelivr.net/npm/docsify-themeable@0/dist/css/theme-simple-dark.css";
-    color.href="color-dark.css";
+    color.href = "color-dark.css";
     element.innerHTML = 'Light Theme';
     element.style.backgroundColor = "#242e33"
     element.style.border = "1px solid #34434b"
