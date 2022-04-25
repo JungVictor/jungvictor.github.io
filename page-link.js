e0005df8b0a002d7a59c5ab5dba0949ff39c2c2e
Victor JUNG <victor.jung@etu.unice.fr>
2022-04-25 15:36:00 +0200

    Pagelink script

--- page-link.js
+++ page-link.js
@@ -3,7 +3,6 @@ function pagelink(hook, vm) {
 	const regex =  /\[pagelink "(.*)":"(.*)"\]/;
 
 	hook.beforeEach(function(content, vm) {
-		content = content.replace(regex, "<div class='page-link'><a href='$2'>$1</a></div>")
 		return content;
 	});
 
